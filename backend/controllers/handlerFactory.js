55f6115b6fec963d7745992f9107014725c0e5cf
Roqeeb <roqeebadeyinka@gmail.com>
2022-09-28 15:24:10 +0100

    filter bugs finally fixed

--- backend/controllers/handlerFactory.js
+++ backend/controllers/handlerFactory.js
@@ -99,43 +99,55 @@ exports.docAction = (Model) =>
   catchAsync(async (req, res, next) => {
     const userId = req.user.id;
     const { action, id } = req.params;
-    console.log([1, 2, 3, 4, 5].filter((x) => x % 2 === 0));
+    // console.log([1, 2, 3, 4, 5].filter((x) => x % 2 === 0));
     const doc = await Model.findById(id);
 
     if (!doc) next(new AppError('No doc with that id', 404));
 
     if (action === 'like') {
       if (doc.likes.includes(userId)) {
-        doc.likes = doc.likes.filter((user) => userId !== user);
+        doc.likes = doc.likes.filter((user) => userId !== user.toString());
       } else {
         doc.likes = doc.likes.push(userId);
       }
+      //
+      //
+      //
+      //
     } else if (action === 'retweet') {
       if (doc.retweet.includes(userId)) {
-        doc.retweet = doc.retweet.filter((user) => userId !== user);
+        doc.retweet = doc.retweet.filter((user) => userId !== user.toString());
       } else {
         doc.retweet = doc.retweet.push(userId);
       }
+      //
+      //
+      //
     } else if (action === 'comment') {
       doc.comment.push({ user: userId, parcel: req.body.comment });
+      //
+      //
+      //
     } else if (action === 'block') {
       if (doc.blocked.includes(userId)) {
-        doc.blocked = doc.blocked.filter((user) => userId === user);
-        req.user.blocked = req.user.blocked.filter((user) => user === id);
+        doc.blocked = doc.blocked.filter((user) => userId !== user.toString());
+        req.user.blocked = req.user.blocked.filter(
+          (user) => user.toString() !== id
+        );
       } else {
         doc.blocked = doc.blocked.push(userId);
         req.user.blocked = req.user.blocked.push(id);
         doc.following = doc.following.filter(
-          (following) => following === userId
+          (following) => following.toString() !== userId
         );
         req.user.following = req.user.following.filter(
-          (following) => following === id
+          (following) => following.toString() !== id
         );
         req.user.followers = req.user.following.filter(
-          (followers) => followers === id
+          (followers) => followers.toString() !== id
         );
         doc.followers = doc.followers.filter(
-          (followers) => followers === userId
+          (followers) => followers.toString() !== userId
         );
       }
       await User.findByIdAndUpdate(req.user.id, {
@@ -143,18 +155,26 @@ exports.docAction = (Model) =>
         followers: req.user.followers,
         blocked: req.user.blocked,
       });
+      //
+      //
+      //
     } else if (action === 'bookmark') {
       if (req.user.bookmarks.includes(id)) {
-        req.user.bookmarks = req.user.bookmarks.filter((tweet) => tweet === id);
+        req.user.bookmarks = req.user.bookmarks.filter(
+          (tweet) => tweet.toString() !== id
+        );
       } else {
         req.user.bookmarks = req.user.bookmarks.push(id);
       }
       await User.findByIdAndUpdate(req.user.id, {
         bookmarks: req.user.bookmarks,
       });
+      //
+      //
+      //
     } else if (action === 'circle') {
       if (req.user.circles.includes(id)) {
-        req.user.circles = req.user.circles.filter((x) => x === id);
+        req.user.circles = req.user.circles.filter((x) => x.toString() !== id);
       } else {
         req.user.circles = req.user.circles.push(id);
       }
