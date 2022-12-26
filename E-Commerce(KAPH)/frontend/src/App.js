cd2bcfb78f73f44dd840d87e686d460a311b5f5c
Adeyinka Roqeeb <roqeebadeyinka@outlook.com>
2022-12-26 16:03:17 +0100

    new commit

--- E-Commerce(KAPH)/frontend/src/App.js
+++ E-Commerce(KAPH)/frontend/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Navbar, Sidebar, Footer } from './components';
+import {
+  HomePage,
+  AboutPage,
+  CartPage,
+  ProductPage,
+  SingleProductPage,
+  ErrorPage,
+  CheckoutPage,
+} from './pages/index';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Sidebar />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route exact path="/cart" element={<CartPage />} />
+          <Route exact path="/about" element={<AboutPage />} />
+          <Route exact path="/product" element={<ProductPage />} />
+          <Route exact path="/product/:id" element={<SingleProductPage />} />
+          <Route exact path="/checkout" element={<CheckoutPage />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </Router>
+      <Footer />
+    </>
   );
 }
 
---
+++ E-Commerce(KAPH)/frontend/src/pages/index.js
@@ -0,0 +1,21 @@
+import AboutPage from './AboutPage';
+import AuthWrapper from './AuthWrapper';
+import CartPage from './CartPage';
+import CheckoutPage from './CheckoutPage';
+import ErrorPage from './ErrorPage';
+import HomePage from './HomePage';
+import PrivateRoute from './PrivateRoute';
+import ProductPage from './ProductsPage';
+import SingleProductPage from './SingleProductPage';
+
+export {
+  AboutPage,
+  AuthWrapper,
+  CheckoutPage,
+  ErrorPage,
+  HomePage,
+  PrivateRoute,
+  ProductPage,
+  SingleProductPage,
+  CartPage,
+};
