e0ee3f112a74d8912718b20bcd63ea7de545664d
Adeyinka Roqeeb <roqeebadeyinka@outlook.com>
2022-12-29 08:22:49 +0100

    new commit

--- E-Commerce(KAPH)/frontend/src/components/Testimonials.js
+++ E-Commerce(KAPH)/frontend/src/components/Testimonials.js
@@ -14,65 +14,26 @@ const Testimonials = () => {
 };
 
 const Wrapper = styled.section`
-  h3,
-  h4 {
-    color: var(--clr-primary-1);
-  }
-  padding: 5rem 0;
-
-  background: var(--clr-primary-10);
-
-  .header h3 {
-    margin-bottom: 2rem;
-  }
-  p {
-    margin-bottom: 0;
-    line-height: 1.8;
-    color: var(--clr-primary-3);
-  }
-  .services-center {
-    margin-top: 4rem;
-    display: grid;
-    gap: 2.5rem;
-  }
-  .service {
-    background: var(--clr-primary-7);
-    text-align: center;
-    padding: 2.5rem 2rem;
-    border-radius: var(--radius);
-    p {
-      color: var(--clr-primary-2);
-    }
-  }
-  span {
-    width: 4rem;
-    height: 4rem;
-    display: grid;
+  width: 100%;
+  height: 50%;
+  .title {
+    text-align: left;
     margin: 0 auto;
-    place-items: center;
-    margin-bottom: 1rem;
-    border-radius: 50%;
-    background: var(--clr-primary-10);
-    color: var(--clr-primary-1);
-    svg {
-      font-size: 2rem;
-    }
+    height: 10%;
+    padding-top: 1rem;
+    padding-left: 1.5rem;
   }
+  .underline {
+    margin-left: 0;
+  }
+
   @media (min-width: 992px) {
-    .header {
-      display: grid;
-      grid-template-columns: 1fr 1fr;
-    }
   }
   @media (min-width: 576px) {
-    .services-center {
-      grid-template-columns: repeat(auto-fit, minmax(360px, 1fr));
-    }
   }
   @media (min-width: 1280px) {
     padding: 0;
     .section-center {
-      transform: translateY(5rem);
     }
   }
 `;
