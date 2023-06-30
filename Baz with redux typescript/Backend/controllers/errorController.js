427c4979aad5ff39a0ec257d3b10b11280d73622
Adeyinka Roqeeb <roqeebadeyinka@outlook.com>
2023-06-30 18:12:16 +0100

    new commit

--- Baz with redux typescript/Backend/controllers/errorController.js
+++ Baz with redux typescript/Backend/controllers/errorController.js
@@ -39,10 +39,6 @@ const sendErrorDev = (err, req, res) => {
 
   // B) RENDERED WEBSITE
   console.error('ERROR 💥', err);
-  return res.status(err.statusCode).render('error', {
-    title: 'Something went wrong!',
-    msg: err.message,
-  });
 };
 
 const sendErrorProd = (err, req, res) => {
@@ -57,30 +53,13 @@ const sendErrorProd = (err, req, res) => {
     }
     // B) Programming or other unknown error: don't leak error details
     // 1) Log error
-    console.error('ERROR 💥', 'fat error');
+    console.error('ERROR 💥', err);
     // 2) Send generic message
     return res.status(500).json({
       status: 'error',
       message: 'Something went very wrong!',
     });
   }
-
-  // // B) RENDERED WEBSITE
-  // // A) Operational, trusted error: send message to client
-  // if (err.isOperational) {
-  //   return res.status(err.statusCode).render('error', {
-  //     title: 'Something went wrong!',
-  //     msg: err.message,
-  //   });
-  // }
-  // // B) Programming or other unknown error: don't leak error details
-  // // 1) Log error
-  // console.error('ERROR 💥', err);
-  // // 2) Send generic message
-  // return res.status(err.statusCode).render('error', {
-  //   title: 'Something went wrong!',
-  //   msg: 'Please try again later.',
-  // });
 };
 
 module.exports = (err, req, res, next) => {
