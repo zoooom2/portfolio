9c70988e2330588ca92f8f3108f46dcade9627bd
Adeyinka Roqeeb <roqeebadeyinka@outlook.com>
2023-07-01 14:45:22 +0100

    set res.cookie properties

--- Baz with redux typescript/Backend/controllers/authControllers.js
+++ Baz with redux typescript/Backend/controllers/authControllers.js
@@ -18,8 +18,8 @@ const createSendToken = (user, statusCode, req, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     httpOnly: true,
-    sameSite: 'lax',
-    secure: req.secure || req.headers['x-forwarded-proto'] === 'https',
+    sameSite: 'none',
+    secure: true,
   });
 
   // Remove password from output
