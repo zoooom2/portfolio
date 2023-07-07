f39f47076fd6b9698bf63d847cf563c06568c667
Adeyinka Roqeeb <roqeebadeyinka@outlook.com>
2023-07-07 18:36:48 +0100

    new commit

--- Baz with redux typescript/frontend/src/features/productFeature/product/ProductImages.tsx
+++ Baz with redux typescript/frontend/src/features/productFeature/product/ProductImages.tsx
@@ -13,14 +13,15 @@ const ProductImages = ({ images = [] }: { images: string[] }) => {
       <div className='gallery'>
         {images.map((image, count) => {
           return (
-            <div>
+            <div
+              className={`${
+                image === images[index] ? 'active' : null
+              } p-3 h-[150px] w-[125px] cursor-pointer object-contain`}>
               <img
                 src={image}
                 alt=''
                 key={count}
-                className={`preview ${
-                  image === images[index] ? 'active' : null
-                }`}
+                className={` ${image === images[index] ? 'active' : null}`}
                 onClick={() => setIndex(count)}
               />
             </div>
@@ -42,42 +43,35 @@ const Wrapper = styled.section`
     flex-wrap: wrap;
     width: 100%;
     gap: 0.5rem;
-
-    .preview {
-      width: 125px;
-      cursor: pointer;
-      height: 150px;
-      object-fit: cover;
-    }
   }
 
   .active {
     box-shadow: 0px 0px 0px 2px var(--clr-primary-5);
   }
 
-  @media (max-width: 576px) {
-    .main {
-      height: 300px;
-      width: auto;
-      object-fit: cover;
-    }
+  // @media (max-width: 576px) {
+  //   .main {
+  //     height: 300px;
+  //     width: auto;
+  //     object-fit: contain;
+  //   }
 
-    .gallery {
-      grid-template-columns: repeat(5, minmax(80px, 1fr));
+  //   .gallery {
+  //     grid-template-columns: repeat(5, minmax(80px, 1fr));
 
-      --gallery-column-gap: 1rem;
-    }
-  }
+  //     --gallery-column-gap: 1rem;
+  //   }
+  // }
 
-  @media (min-width: 992px) {
-    .main {
-      height: 400px;
-      width: auto;
-    }
+  // @media (min-width: 992px) {
+  //   .main {
+  //     height: 400px;
+  //     width: auto;
+  //   }
 
-    .gallery {
-      --gallery-column-gap: 1rem;
-    }
+  //   .gallery {
+  //     --gallery-column-gap: 1rem;
+  //   }
   }
 `;
 
