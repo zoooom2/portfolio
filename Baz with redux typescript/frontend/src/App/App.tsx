7944d9383f8d676fa29926ac5d98e0c177236906
Adeyinka Roqeeb <roqeebadeyinka@outlook.com>
2023-09-26 18:55:50 +0100

    splitting code

--- Baz with redux typescript/frontend/src/App/App.tsx
+++ Baz with redux typescript/frontend/src/App/App.tsx
@@ -12,29 +12,29 @@ import {
 } from '../features/userFeature/userSlice';
 import { Navbar, Sidebar, ProtectedRoute, Loading } from '../global_components';
 
-import {
-  SingleProductPage,
-  ThesisPage,
-  ErrorPage,
-  ContactPage,
-  OrderPage,
-  Signup,
-  LoginPage,
-  PaymentGateway,
-  CheckoutPage,
-} from '../pages';
+import // SingleProductPage,
+// ThesisPage,
+// ErrorPage,
+// ContactPage,
+// OrderPage,
+// Signup,
+// LoginPage,
+// PaymentGateway,
+// CheckoutPage,
+'../pages';
 
 const HomePage = lazy(() => import('../pages/HomePage'));
-// const ThesisPage = lazy(() => import('../pages/ThesisPage'));
+const ThesisPage = lazy(() => import('../pages/ThesisPage'));
 const CartPage = lazy(() => import('../pages/CartPage'));
 const ProductPage = lazy(() => import('../pages/ProductsPage'));
-// const ErrorPage = lazy(() => import('../pages/ErrorPage'));
-// const CheckoutPage = lazy(() => import('../pages/CheckoutPage'));
-// const PaymentGateway = lazy(() => import('../pages/PaymentGatewayPage'));
-// const LoginPage = lazy(() => import('../pages/LoginPage'));
-// const Signup = lazy(() => import('../pages/Signup'));
-// const OrderPage = lazy(() => import('../pages/OrderPage'));
-// const ContactPage = lazy(() => import('../pages/ContactPage'));
+const ErrorPage = lazy(() => import('../pages/ErrorPage'));
+const CheckoutPage = lazy(() => import('../pages/CheckoutPage'));
+const PaymentGateway = lazy(() => import('../pages/PaymentGatewayPage'));
+const LoginPage = lazy(() => import('../pages/LoginPage'));
+const Signup = lazy(() => import('../pages/Signup'));
+const OrderPage = lazy(() => import('../pages/OrderPage'));
+const ContactPage = lazy(() => import('../pages/ContactPage'));
+const SingleProductPage = lazy(() => import('../pages/SingleProductPage'));
 const AdminPages = lazy(() => import('../pages/AdminPages'));
 
 import AdminRoutes from '../features/adminFeature/admin/AdminRoutes';
