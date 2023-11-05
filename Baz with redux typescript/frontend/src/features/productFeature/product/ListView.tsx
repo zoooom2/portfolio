1ff31fe95247ed5ca856897f7f3fc5b4cf681771
Adeyinka Roqeeb <roqeebadeyinka@outlook.com>
2023-11-05 22:20:47 +0100

    new commit

--- Baz with redux typescript/frontend/src/features/productFeature/product/ListView.tsx
+++ Baz with redux typescript/frontend/src/features/productFeature/product/ListView.tsx
@@ -11,7 +11,7 @@ const ListView = ({ products }: { products: SingleProductType[] }) => {
           <Link
             to={`/shop/${id}`}
             key={id}
-            className='border-b border-black flex flex-col h-[50vh]'>
+            className='border-b border-black flex flex-col h-[50vh] px-[24px] py-2'>
             <figure className='place-items-center h-3/4 flex justify-center'>
               <img
                 src={images[0]}
@@ -19,9 +19,11 @@ const ListView = ({ products }: { products: SingleProductType[] }) => {
                 className='aspect-[230/317] h-full object-contain'
               />
             </figure>
-            <div className='h-1/4'>
-              <h4>{name}</h4>
-              <h5 className='price'>{`₦${price}`}</h5>
+            <div className='h-1/4 flex flex-col gap-2'>
+              <h4 className='text-baz-black font-baz2 text-[16px] font-semibold tracking-[1.6px]'>
+                {name}
+              </h4>
+              <h5 className='font-baz1 text-[16px]'>{`₦${price}`}</h5>
             </div>
           </Link>
         );
