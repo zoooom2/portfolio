84c7206758b7e537db1c2534c8aa798f58d0738e
Adeyinka Roqeeb <roqeebadeyinka@outlook.com>
2023-11-05 20:33:52 +0100

    new commit

--- Baz with redux typescript/frontend/src/utils/constants.tsx
+++ Baz with redux typescript/frontend/src/utils/constants.tsx
@@ -161,11 +161,11 @@ export const priceFormat = (price: number) =>
   `₦${new Intl.NumberFormat('currency').format(price)}`;
 
 export const featuredSlide1 = [
-  'https://res.cloudinary.com/dlk2a6ppp/image/upload/w_500/v1695742709/image3_z9xdzm.jpg',
-  'https://res.cloudinary.com/dlk2a6ppp/image/upload/w_500/v1695742709/img5_ewodvu.jpg',
-  'https://res.cloudinary.com/dlk2a6ppp/image/upload/w_500/v1695742708/img7_yoain9.jpg',
-  'https://res.cloudinary.com/dlk2a6ppp/image/upload/w_500/v1695742707/img6_rhoszr.jpg',
-  'https://res.cloudinary.com/dlk2a6ppp/image/upload/w_500/v1695742707/img4_u6rumj.jpg',
+  'https://res.cloudinary.com/dlk2a6ppp/image/upload/w_900/v1695742709/image3_z9xdzm.jpg',
+  'https://res.cloudinary.com/dlk2a6ppp/image/upload/w_900/v1695742709/img5_ewodvu.jpg',
+  'https://res.cloudinary.com/dlk2a6ppp/image/upload/w_900/v1695742708/img7_yoain9.jpg',
+  'https://res.cloudinary.com/dlk2a6ppp/image/upload/w_900/v1695742707/img6_rhoszr.jpg',
+  'https://res.cloudinary.com/dlk2a6ppp/image/upload/w_900/v1695742707/img4_u6rumj.jpg',
 ];
 export const featuredSlide2 = [
   'https://res.cloudinary.com/dlk2a6ppp/image/upload/w_500/v1695742707/img3_owfvt7.jpg',
