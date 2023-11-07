97d65ad08b7fe3c12aec7f9c7cdb19591f0d1264
Adeyinka Roqeeb <roqeebadeyinka@outlook.com>
2023-11-07 15:39:25 +0100

    new commit

--- Baz with redux typescript/frontend/src/pages/ProductsPage.tsx
+++ Baz with redux typescript/frontend/src/pages/ProductsPage.tsx
@@ -7,7 +7,6 @@ import {
   loadProducts,
   sortProduct,
   updateFilters,
-  updateProductCollection,
 } from '../features/filterFeature/filterSlice';
 import { fetchProducts } from '../features/productFeature/productSlice';
 import { products_url as url } from '../utils/constants';
@@ -17,8 +16,9 @@ import ProductList from '../features/productFeature/product/ProductList';
 
 const ProductsPage = () => {
   const dispatch = useAppDispatch();
-  const { all_products, filtered_product, filtered_collection, sort, filters } =
-    useAppSelector((state) => state.filter);
+  const { all_products, filtered_product, sort, filters } = useAppSelector(
+    (state) => state.filter
+  );
   const { products } = useAppSelector((state) => state.product);
 
   useEffect(() => {
@@ -37,7 +37,7 @@ const ProductsPage = () => {
     dispatch(loadProducts(products));
   }, [products]);
 
-  const categories = getUniqueValues(filtered_collection, 'category');
+  const categories = getUniqueValues(filtered_product, 'category');
   const collections = getUniqueValues(all_products, 'collectionName');
 
   return (
@@ -47,12 +47,11 @@ const ProductsPage = () => {
           <select
             name='collection'
             className='font-baz2 text-[20px] font-semibold tracking-[2px] bg-baz-white outline-none'
-            onChange={(e) => {
+            onChange={(e) =>
               dispatch(
                 updateFilters({ name: 'collection', value: e.target.value })
-              );
-              dispatch(updateProductCollection(filtered_product));
-            }}>
+              )
+            }>
             <option value={'all'}>All Collection</option>
             {collections.map((collection, index) => (
               <option value={collection} key={index}>
