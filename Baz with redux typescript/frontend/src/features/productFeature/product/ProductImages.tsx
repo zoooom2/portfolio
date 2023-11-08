1b7c4ee6f7ffa6efdc3f8034f9545d243c69ac91
Adeyinka Roqeeb <roqeebadeyinka@outlook.com>
2023-11-08 06:07:04 +0100

    new commit

--- Baz with redux typescript/frontend/src/features/productFeature/product/ProductImages.tsx
+++ Baz with redux typescript/frontend/src/features/productFeature/product/ProductImages.tsx
@@ -41,6 +41,7 @@ const ProductImages = ({ images = [] }: { images: string[] }) => {
 
   return (
     <Swiper
+      className='w-9'
       spaceBetween={50}
       slidesPerView={1}
       modules={[Pagination]}
