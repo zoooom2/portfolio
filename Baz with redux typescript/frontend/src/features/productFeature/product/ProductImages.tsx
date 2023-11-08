105ca1793ba84da8af2b6e07191dbeb5601adf19
Adeyinka Roqeeb <roqeebadeyinka@outlook.com>
2023-11-08 05:08:32 +0100

    new commit

--- Baz with redux typescript/frontend/src/features/productFeature/product/ProductImages.tsx
+++ Baz with redux typescript/frontend/src/features/productFeature/product/ProductImages.tsx
@@ -1,84 +1,55 @@
-import { useState } from 'react';
-import styled from 'styled-components';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
 
 const ProductImages = ({ images = [] }: { images: string[] }) => {
-  const [index, setIndex] = useState(0);
+  //   return (
+  //     <Wrapper className='p-5'>
+  //       <div className='w-full h-auto flex justify-center p-8'>
+  //         <img
+  //           loading='lazy'
+  //           src={images[index]}
+  //           alt='main'
+  //           className='object-contain h-full'
+  //         />
+  //       </div>
+
+  //       <div className='gallery'>
+  //         {images.map((image, count) => {
+  //           return (
+  //             <div
+  //               className={`flex items-center ${
+  //                 image === images[index] ? 'active' : null
+  //               } p-3 h-[130px] w-[125px] cursor-pointer object-contain`}>
+  //               <img
+  //                 src={image}
+  //                 loading='lazy'
+  //                 alt=''
+  //                 key={count}
+  //                 onClick={() => setIndex(count)}
+  //               />
+  //             </div>
+  //           );
+  //         })}
+  //       </div>
+  //     </Wrapper>
+  //   );
+  // };
 
   return (
-    <Wrapper className='p-5'>
-      <div className='w-full h-auto flex justify-center p-8'>
-        <img
-          loading='lazy'
-          src={images[index]}
-          alt='main'
-          className='object-contain h-full'
-        />
-      </div>
-
-      <div className='gallery'>
-        {images.map((image, count) => {
-          return (
-            <div
-              className={`flex items-center ${
-                image === images[index] ? 'active' : null
-              } p-3 h-[130px] w-[125px] cursor-pointer object-contain`}>
-              <img
-                src={image}
-                loading='lazy'
-                alt=''
-                key={count}
-                onClick={() => setIndex(count)}
-              />
-            </div>
-          );
-        })}
-      </div>
-    </Wrapper>
+    <Swiper
+      spaceBetween={50}
+      slidesPerView={1}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}>
+      {images.map((image, index) => (
+        <SwiperSlide key={index}>
+          <img src={image} alt='' />
+        </SwiperSlide>
+      ))}
+    </Swiper>
   );
 };
-
-const Wrapper = styled.section`
-  display: grid;
-  place-items: center;
-
-  .gallery {
-    margin-top: 1rem;
-    display: flex;
-    justify-content: flex-start;
-    flex-wrap: wrap;
-    width: 100%;
-    gap: 0.5rem;
-  }
-
-  .active {
-    background-color:white; 
-    box-shadow:var(--light-shadow);
-  }
-
-  // @media (max-width: 576px) {
-  //   .main {
-  //     height: 300px;
-  //     width: auto;
-  //     object-fit: contain;
-  //   }
-
-  //   .gallery {
-  //     grid-template-columns: repeat(5, minmax(80px, 1fr));
-
-  //     --gallery-column-gap: 1rem;
-  //   }
-  // }
-
-  // @media (min-width: 992px) {
-  //   .main {
-  //     height: 400px;
-  //     width: auto;
-  //   }
-
-  //   .gallery {
-  //     --gallery-column-gap: 1rem;
-  //   }
-  }
-`;
-
 export default ProductImages;
