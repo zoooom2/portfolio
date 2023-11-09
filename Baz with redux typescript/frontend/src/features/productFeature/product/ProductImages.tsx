2b03567cef6e6924bcac1dced42a8296ea4e14d0
Adeyinka Roqeeb <roqeebadeyinka@outlook.com>
2023-11-09 05:52:37 +0100

    new commit

--- Baz with redux typescript/frontend/src/features/productFeature/product/ProductImages.tsx
+++ Baz with redux typescript/frontend/src/features/productFeature/product/ProductImages.tsx
@@ -1,25 +1,10 @@
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination } from 'swiper/modules';
-
-// Import Swiper styles
-import 'swiper/swiper.min.css';
+import Carousel from 'nuka-carousel';
 
 const ProductImages = ({ images = [] }: { images: string[] }) => {
-  return (
-    <Swiper
-      spaceBetween={50}
-      slidesPerView={1}
-      modules={[Pagination]}
-      pagination={{ clickable: true }}
-      onSlideChange={() => console.log('slide change')}
-      onSwiper={(swiper) => console.log(swiper)}>
-      {images.map((image, index) => (
-        <SwiperSlide key={index}>
-          <img src={image} alt='' />
-        </SwiperSlide>
-      ))}
-    </Swiper>
-  );
+  <Carousel>
+    {images.map((image, index) => (
+      <img key={index} src={image} alt='' />
+    ))}
+  </Carousel>;
 };
 export default ProductImages;
