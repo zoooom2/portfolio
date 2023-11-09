bedf079de08ccf5bf3a7bc0d6bae029670634c52
Adeyinka Roqeeb <roqeebadeyinka@outlook.com>
2023-11-09 06:16:22 +0100

    new commit

--- Baz with redux typescript/frontend/src/features/productFeature/product/ProductImages.tsx
+++ Baz with redux typescript/frontend/src/features/productFeature/product/ProductImages.tsx
@@ -2,7 +2,7 @@ import Carousel from 'nuka-carousel';
 
 const ProductImages = ({ images = [] }: { images: string[] }) => {
   return (
-    <Carousel withoutControls={true} className='h-[40vh]'>
+    <Carousel withoutControls={true} className='h-[40vh] tablet:h-screen'>
       {images.map((image, index) => (
         <img key={index} src={image} alt='' className='w-full object-contain' />
       ))}
