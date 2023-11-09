a6f584a7f2fd971d0c8db91e4cebac2f3cbdc63b
Adeyinka Roqeeb <roqeebadeyinka@outlook.com>
2023-11-09 05:56:16 +0100

    new commit

--- Baz with redux typescript/frontend/src/features/productFeature/product/ProductImages.tsx
+++ Baz with redux typescript/frontend/src/features/productFeature/product/ProductImages.tsx
@@ -1,10 +1,12 @@
 import Carousel from 'nuka-carousel';
 
 const ProductImages = ({ images = [] }: { images: string[] }) => {
-  <Carousel>
-    {images.map((image, index) => (
-      <img key={index} src={image} alt='' />
-    ))}
-  </Carousel>;
+  return (
+    <Carousel>
+      {images.map((image, index) => (
+        <img key={index} src={image} alt='' />
+      ))}
+    </Carousel>
+  );
 };
 export default ProductImages;
