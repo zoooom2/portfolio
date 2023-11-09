fc750502f7a51bcf444a0e6eb2672afc678b3b9a
Adeyinka Roqeeb <roqeebadeyinka@outlook.com>
2023-11-09 07:50:26 +0100

    new commit

--- Baz with redux typescript/frontend/src/features/productFeature/product/ProductImages.tsx
+++ Baz with redux typescript/frontend/src/features/productFeature/product/ProductImages.tsx
@@ -1,8 +1,27 @@
 import Carousel from 'nuka-carousel';
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
+
+type voidFunc = () => void;
 
 const ProductImages = ({ images = [] }: { images: string[] }) => {
   return (
-    <Carousel withoutControls={false} className='h-full '>
+    <Carousel
+      withoutControls={false}
+      renderCenterLeftControls={({
+        previousSlide,
+      }: {
+        previousSlide: voidFunc;
+      }) => (
+        <button onClick={previousSlide}>
+          <MdKeyboardArrowLeft />
+        </button>
+      )}
+      renderCenterRightControls={({ nextSlide }: { nextSlide: voidFunc }) => (
+        <button onClick={nextSlide}>
+          <MdKeyboardArrowRight />
+        </button>
+      )}
+      className='h-full '>
       {images.map((image, index) => (
         <img key={index} src={image} alt='' className='w-full object-contain' />
       ))}
