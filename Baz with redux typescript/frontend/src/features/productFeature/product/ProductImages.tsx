7e8e275e8739ca1737c30575f3d793c94554b10c
Adeyinka Roqeeb <roqeebadeyinka@outlook.com>
2023-11-09 07:05:54 +0100

    new commit

--- Baz with redux typescript/frontend/src/features/productFeature/product/ProductImages.tsx
+++ Baz with redux typescript/frontend/src/features/productFeature/product/ProductImages.tsx
@@ -2,9 +2,16 @@ import Carousel from 'nuka-carousel';
 
 const ProductImages = ({ images = [] }: { images: string[] }) => {
   return (
-    <Carousel withoutControls={true} className='h-full'>
+    <Carousel className='h-full'>
       {images.map((image, index) => (
-        <img key={index} src={image} alt='' className='w-full object-contain' />
+        <div className='pb-4 flex items-center justify-center'>
+          <img
+            key={index}
+            src={image}
+            alt=''
+            className='w-full object-contain'
+          />
+        </div>
       ))}
     </Carousel>
   );
