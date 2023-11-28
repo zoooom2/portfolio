eb16b4a861dd8705a8c73545508e16a4e99afbdb
Adeyinka Roqeeb <roqeebadeyinka@outlook.com>
2023-11-28 13:22:12 +0100

    new commit

--- Baz with redux typescript/frontend/src/features/adminFeature/admin/AdminNav.tsx
+++ Baz with redux typescript/frontend/src/features/adminFeature/admin/AdminNav.tsx
@@ -1,18 +1,18 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import logo from '../../../assets/image 2.svg';
 
 import { useAppDispatch } from '../../../App/hooks';
 import AdminMenuButtons from './AdminMenuButtons';
 import { FaBars } from 'react-icons/fa';
 import { openAdminSidebar } from '../adminSlice';
+import { BAZLogo } from '../../../utils/constants';
 
 const AdminNav = () => {
   const dispatch = useAppDispatch();
   return (
     <Wrapper className='fixed w-full flex pt-4 pb-3 border-b border-solid border-[#b6b6b6] items-center justify-between px-8 bg-white z-10'>
       <Link to='/admin/overview'>
-        <img src={logo} alt='logo' className='logo' />
+        <img src={BAZLogo} alt='logo' className='logo' />
       </Link>
       <div className='max-tablet:hidden'>
         <AdminMenuButtons />
