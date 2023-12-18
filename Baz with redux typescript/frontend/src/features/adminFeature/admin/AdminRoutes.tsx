b9df082efffb504898d80a5fbc36212b9c4aa2bf
Adeyinka Roqeeb <roqeebadeyinka@outlook.com>
2023-12-18 17:09:48 +0100

    new commit

--- Baz with redux typescript/frontend/src/features/adminFeature/admin/AdminRoutes.tsx
+++ Baz with redux typescript/frontend/src/features/adminFeature/admin/AdminRoutes.tsx
@@ -1,14 +1,13 @@
 import { Navigate, Outlet, useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
-import { useAppDispatch, useAppSelector } from '../../../App/hooks';
+import { useAppDispatch } from '../../../App/hooks';
 import { fetchProfile } from '../../userFeature/userSlice';
-import { Loading } from '../../../global_components';
+
 import { setAdminRoute } from '../adminSlice';
 
 const AdminRoutes = ({ isAuthenticated }: { isAuthenticated: boolean }) => {
   const location = useLocation();
   const dispatch = useAppDispatch();
-  const { loading } = useAppSelector((state) => state.user);
 
   useEffect(() => {
     if (!isAuthenticated) dispatch(fetchProfile());
