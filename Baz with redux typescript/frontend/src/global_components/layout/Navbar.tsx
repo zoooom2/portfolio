b99d30fa1491e649882bc4933227c9bcaf0ca06e
Adeyinka Roqeeb <roqeebadeyinka@outlook.com>
2023-12-30 12:24:24 +0100

    new commit

--- Baz with redux typescript/frontend/src/global_components/layout/Navbar.tsx
+++ Baz with redux typescript/frontend/src/global_components/layout/Navbar.tsx
@@ -35,9 +35,7 @@ const Nav = ({ buttons, admin }: { buttons: JSX.Element; admin: boolean }) => {
     <NavContainer className='place-center bg-baz-white flex flex-col gap-4 pb-[18px]'>
       <div className={admin ? 'admin-nav-center' : 'nav-center'}>
         <div className='flex items-center justify-between'>
-          <Link
-            to={adminRoute ? '/admin' : '/'}
-            className='h-[80px] border-rose-500 border'>
+          <Link to={adminRoute ? '/admin' : '/'} className='h-[80px]'>
             <img src={BAZLogo} alt='logo' className='object-contain h-full' />
           </Link>
           <div className='nav-toggle'>
