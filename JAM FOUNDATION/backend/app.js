676a5134e690dd0d2718ee976955a1ce760075b6
Roqeeb Adeyinka <richierichie173@gmail.com>
2024-06-06 16:06:12 +0100

    updated the cors

--- JAM FOUNDATION/backend/app.js
+++ JAM FOUNDATION/backend/app.js
@@ -29,7 +29,7 @@ app.enable('trust proxy');
 // Implement CORS
 app.use(
   cors({
-    origin: ['http://localhost:5173', 'https://jamfoundation.vercel.app',  'https://jamfoundation.org.ng'],
+    origin: ['http://localhost:5173', 'https://jamfoundation.vercel.app',  'https://www.jamfoundation.org.ng'],
     methods: 'GET,POST,PATCH,DELETE',
     credentials: true,
   }),
