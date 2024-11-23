5cdf7682f042f9d6106fc4a6f58f47208af3275e
Roqeeb Adeyinka <richierichie173@gmail.com>
2024-11-23 20:28:54 +0100

    new commit

--- Baz with redux typescript/frontend/src/features/userFeature/user/UserRoutes.tsx
+++ Baz with redux typescript/frontend/src/features/userFeature/user/UserRoutes.tsx
@@ -1,28 +1,22 @@
 import { Outlet } from 'react-router-dom';
-import { fetchProfile, setClicked } from '../userSlice';
+import { setClicked } from '../userSlice';
 import { useEffect } from 'react';
 import { useAppDispatch, useAppSelector } from '../../../App/hooks';
 import { setAdminRoute } from '../../adminFeature/adminSlice';
-import { Loading, Navbar, Sidebar } from '../../../global_components';
+import { Navbar, Sidebar } from '../../../global_components';
 import { CartButtons } from '../../cartFeature/cart';
 import { links } from '../../../utils/constants';
 
 const UserRoutes = () => {
 	const dispatch = useAppDispatch();
 	const { clicked } = useAppSelector((state) => state.user);
-	const { isAuthenticated, loading } = useAppSelector((state) => state.user);
+
 	useEffect(() => {
 		if (clicked) dispatch(setClicked(true));
 		dispatch(setAdminRoute(false));
 	}, []);
 
-	useEffect(() => {
-		dispatch(fetchProfile());
-	}, []);
-
-	return loading ? (
-		<Loading />
-	) : isAuthenticated ? (
+	return (
 		<>
 			<Navbar
 				buttons={<CartButtons />}
@@ -33,13 +27,6 @@ const UserRoutes = () => {
 				footerButtons={<CartButtons />}
 			/>
 			<Outlet />
-		</>
-	) : (
-		<>
-			<div className='h-screen w-screen flex items-center justify-center gap-[24px] font-baz1 flex-col'>
-				<div className='font-extrabold text-[48px] font-baz1'>SATURDAY</div>
-				<div className='text-[32px] font-semibold'>8:00PM</div>
-			</div>
 		</>
 	);
 };
